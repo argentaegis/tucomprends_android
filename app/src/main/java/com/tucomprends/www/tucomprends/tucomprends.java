678be66cdaba5df9b85725d4fe38ecc711617669
Andrew Jones <argentaegis@gmail.com>
2018-03-12 16:07:57 -0500

    First Commmit

---
+++ app/src/main/java/com/tucomprends/www/tucomprends/tucomprends.java
@@ -0,0 +1,68 @@
+package com.tucomprends.www.tucomprends;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+
+
+public class tucomprends extends AppCompatActivity {
+
+    private ActionBar toolbar;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+
+                case R.id.navigation_translate_text:
+                    toolbar.setTitle("Tu Comprends: Text");
+                    fragment = new Translate_Text_Fragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_translate_voice:
+                    toolbar.setTitle("Tu Comprends: Voice");
+                    fragment = new Translate_Voice_Fragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_translate_image:
+                    toolbar.setTitle("Tu Comprends: Image");
+                    fragment = new Translate_Image_Fragment();
+                    loadFragment(fragment);
+                    return true;
+
+
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tucomprends);
+
+        toolbar = getSupportActionBar();
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        toolbar.setTitle("Tu Comprends: Text");
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+}
